790dbd93ccb5ff80b224c811727320a0613fedc4
JoseLeonTM <jleon@tekmexico.com>
2017-07-25 17:32:32 -0700

    Working on exercise 331

---
+++ 331/331.js
@@ -0,0 +1,8 @@
+var op1 = '12+4-1';
+var op2 = "5-2*2";
+function execute(op) {
+    var result, numbers = [];
+    return result;
+}
+console.log(12 + 4 - 1);
+console.log(5 - 2 * 2);
---
+++ 331/331.ts
@@ -0,0 +1,12 @@
+
+var op1 = '12+4-1';
+var op2 = "5-2*2";
+
+function execute(op: string): number {
+    var result: number,
+        numbers: number[] = [];
+    
+    return result;
+}
+console.log(12 + 4 - 1);
+console.log(5 - 2 * 2);
